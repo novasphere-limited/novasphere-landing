452c95222efe6fc8b7d9de2e074b28af6a13a346
AdegbiteRazaq <adegbiterasaq101@gmail.com>
2025-02-08 14:07:58 +0100

    Fixed social links

--- components/LinkBtn.jsx
+++ components/LinkBtn.jsx
@@ -4,7 +4,7 @@ export default function LinkBtn({ href, text }) {
   return (
     <>
       <Link href={href}>
-        <button className="md:text-2xl text-xl font-medium md:px-[48px] md:py-[10px] sm:px-[30px] px-3 py-3 bg-warning600 text-white rounded-md sm:w-[431px] w-full">
+        <button className="md:text-2xl text-xl font-medium md:px-[48px] md:py-[10px] sm:px-[30px] px-3 py-3 bg-warning600 hover:bg-[#1f76d3] hover:shadow-md transition-all duration-300 ease-in-out text-white rounded-md sm:w-[431px] w-full">
           {text}
         </button>
       </Link>
--- components/SocialPage.jsx
+++ components/SocialPage.jsx
@@ -6,30 +6,59 @@ import {
   faTwitter,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function SocialMedia({ color }) {
   return (
     <div className="flex gap-2">
-      <FontAwesomeIcon
-        icon={faLinkedin}
-        style={{ color: color, borderColor: color }}
-        className="social-icon"
-      />
-      <FontAwesomeIcon
-        icon={faFacebook}
-        style={{ color: color, borderColor: color }}
-        className="social-icon"
-      />
-      <FontAwesomeIcon
-        icon={faInstagram}
-        style={{ color: color, borderColor: color }}
-        className="social-icon"
-      />
-      <FontAwesomeIcon
-        icon={faTwitter}
-        style={{ color: color, borderColor: color }}
-        className="social-icon"
-      />
+      <Link
+        href="https://www.linkedin.com/company/106208273/admin/settings/manage-admins/"
+        passHref
+        legacyBehavior
+      >
+        <a target="_blank">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            style={{ color: color, borderColor: color }}
+            className="social-icon"
+          />
+        </a>
+      </Link>
+      <Link
+        href="https://web.facebook.com/people/Novasphere/61572540403287/"
+        passHref
+        legacyBehavior
+      >
+        <a target="_blank">
+          <FontAwesomeIcon
+            icon={faFacebook}
+            style={{ color: color, borderColor: color }}
+            className="social-icon"
+          />
+        </a>
+      </Link>
+      <Link
+        href="https://www.instagram.com/novasphere.tech?fbclid=IwY2xjawIQafRleHRuA2FlbQIxMAABHXP2Cr7dH-rTW7ar_gR3Kmolbfs1i1UvVK7USw_bI-FKyU7A4WLEmbqGcg_aem_tkZGGvvaULpZ08eMEeZACA"
+        passHref
+        legacyBehavior
+      >
+        <a target="_blank">
+          <FontAwesomeIcon
+            icon={faInstagram}
+            style={{ color: color, borderColor: color }}
+            className="social-icon"
+          />
+        </a>
+      </Link>
+      <Link href="https://x.com/Novasphere_tech" passHref legacyBehavior>
+        <a target="_blank">
+          <FontAwesomeIcon
+            icon={faTwitter}
+            style={{ color: color, borderColor: color }}
+            className="social-icon"
+          />
+        </a>
+      </Link>
     </div>
   );
 }
