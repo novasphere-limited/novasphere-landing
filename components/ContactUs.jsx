1acee65b2edcc16012ef32c7f5e0c456f3407f1d
abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2025-05-04 20:14:24 +0100

    Just lil UI fixes

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -30,13 +30,11 @@ export default function ContactUs() {
     contactDataPayload(formData);
   };
 
-  console.log(formData);
-
   return (
     <div className="main-container sm:flex justify-between items-center md:mt-[59px] mt-[45px] contact-gap xl:mb-[26.5rem] mb-[100px] md:mb-[180px] lg:mb-[250px]">
       <div className="w-full">
         <h1 className="font-normal text-[32px]">Hello,</h1>
-        <p className="font-base mb-7 lg:mb-10 xl:mb-[60px]">
+        <p className="font-base mb-6">
           Send a message, Let&rsquo;s talk about your project
         </p>
         <form
@@ -46,41 +44,36 @@ export default function ContactUs() {
             handleSubmit();
           }}
         >
-          <label className="text-base">Full Name</label>
           <input
             type="text"
-            placeholder=""
+            placeholder="Full Name"
             className="w-full border-b mb-[34px] bg-transparent border-[#D2D2D2] py-2 input-contact"
             value={formData?.fullName}
             onChange={(e) => handleChange(e)}
             name="fullName"
             required
           />
-          <label className="text-base my-5">Email Address</label>
-
           <input
             type="email"
-            placeholder=""
+            placeholder="Email Address"
             className="w-full border-b border-[#D2D2D2] mb-[34px] bg-transparent py-2 input-contact"
             value={formData?.emailAddress}
             onChange={(e) => handleChange(e)}
             name="emailAddress"
             required
           />
-          <label className="text-base">Your Message</label>
-
           <input
             type="text"
-            placeholder=""
+            placeholder="Your Message"
             className="w-full border-b border-[#D2D2D2] mb-[34px] bg-transparent py-2 !px-2 input-contact"
             value={formData?.message}
             onChange={(e) => handleChange(e)}
             name="message"
             required
           />
-          <div className=" lg:mt-[59px] mt-7 flex items-center justify-center">
+          <div className=" lg:mt-[59px] mt-7">
             <button
-              className="hover:bg-[#1f76d3] max-w-[300px] w-full text-white bg-[#3090F8] text-sm font-semibold py-[10px] rounded-lg"
+              className="hover:bg-[#1f76d3] md:w-[300px] w-full text-white bg-[#3090F8] text-sm font-semibold py-[10px] rounded-lg"
               type={"submit"}
               disabled={contactDataIsLoading}
             >
